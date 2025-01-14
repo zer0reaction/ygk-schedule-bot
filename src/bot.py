c76afb4d8e9e3cc73be838e410203187d0b7de92
zer0reaction <boltgopnik@gmail.com>
2025-01-14 21:22:49 +0300

    i hate this

--- src/bot.py
+++ src/bot.py
@@ -1,21 +1,48 @@
 from telebot import TeleBot
-from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
+from telebot.types import InlineKeyboardButton, ReplyKeyboardMarkup
 from creds import token
 from web import *
 from database import *
 
+# initializing some important variables
 bot = TeleBot(token=token, parse_mode="HTML")
+groups = get_groups()
+
+buttons = ReplyKeyboardMarkup(resize_keyboard=True)
+for group in groups:
+    #                                group_name 
+    buttons.add(InlineKeyboardButton(group[1]))
+
+greeting_text="Выберите группу из списка:\n\nНИКАКОЙ ГАРАНТИИ ПРАВИЛЬНОСТИ ДАННЫХ И РАБОТОСПОСОБНОСТИ БОТА НЕ ПРЕДОСТАВЛЯЕТСЯ"
+user_already_in_db_text="Вы уже в базе данных.\nИспользуйте /reset для сброса данных о себе"
+# -------------------------------------
+
+
+def send_current_schedule(id: int):
+    markup=InlineKeyboardMarkup()
+    markup.add(InlineKeyboardButton(text="Расписание", callback_data="schedule"))
+    return bot.send_message(chat_id=id, text="placeholder", reply_markup=markup).id
+
 
 @bot.message_handler(commands=["start"])
 def start(message):
     id = message.from_user.id
-    buttons = InlineKeyboardMarkup()
-    groups = get_groups()
 
-    for group in groups:
-        buttons.add(InlineKeyboardButton(group[1],
-                                         callback_data="useradd_{}_{}".format(id, group[0])))
+    if check_user_existence(id):
+        send_current_schedule(id)
+    else:
+        bot.send_message(chat_id=id, text=greeting_text, reply_markup=buttons)
 
-    bot.send_message(chat_id=id, text="test", reply_markup=buttons)
+
+@bot.message_handler(commands=["reset"])
+def reset(message):
+    id = message.from_user.id
+
+    if check_user_existence(id):
+        delete_user(id)
+        bot.send_message(chat_id=id, text="Вы были успешно удалены из базы данных. Для продожения введите /start")
+    else:
+        bot.send_message(chat_id=id, text="Вы не в базе данных")
+
 
 bot.infinity_polling()
--- src/database.py
+++ src/database.py
@@ -1,20 +1,51 @@
 from sqlite3 import connect
 
+
 def get_groups():
     con = connect("db/base.db")
     cur = con.cursor()
-
     cur.execute("select * from groups")
     all = cur.fetchall()
-
     con.close()
     return all
 
-def add_user(id: int, group: str):
+
+def check_user_existence(id: int):
     con = connect("db/base.db")
     cur = con.cursor()
+    cur.execute("select exists(select 1 from users where telegram_id = {})".format(id))
+    result = cur.fetchone()[0] == 1
+    con.close()
+    return result
 
-    cur.execute("insert into users values ({}, {})".format(id, group))
 
+def get_message_id(id: int):
+    con = connect("db/base.db")
+    cur = con.cursor()
+    cur.execute("select message_id from users where telegram_id = {}".format(id))
+    result = cur.fetchone()[0]
+    con.close()
+    return result
+
+
+def update_message_id(telegram_id: int, message_id: int):
+    con = connect("db/base.db")
+    cur = con.cursor()
+    cur.execute("update users set message_id = {} where telegram_id = {} ".format(message_id, telegram_id))
+    con.commit()
+    con.close()
+
+
+def add_user(id: int, group_id: int, message_id: int):
+    con = connect("db/base.db")
+    cur = con.cursor()
+    cur.execute("insert into users values ({}, {}, {})".format(id, group_id, message_id))
+    con.commit()
+    con.close()
+
+def delete_user(id: int):
+    con = connect("db/base.db")
+    cur = con.cursor()
+    cur.execute("delete from users where telegram_id = {}".format(id))
     con.commit()
     con.close()
